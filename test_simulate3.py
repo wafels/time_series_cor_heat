90f0f42ae07d39a3ecd21a800a5faa36e9b5bcc9
wafels <jackreland@gmail.com>
2020-03-06 17:07:53 -0500

    more work on getting the fitting code right

--- test_simulate3.py
+++ test_simulate3.py
@@ -23,9 +23,17 @@ log_lorentz = LogLorentz1D()
 
 # Power law plus constant plus Gaussian
 observation_model = log_lorentz
+observation_model.name = 'log_lorentz'
+
+observation_model = power_law + constant + log_lorentz
+observation_model.name = 'pl_c_ll'
+
 
 
 # parameters for fake data.
+
+# Frequency range (normalized so the first one is always 1
+freq = np.linspace(0.01, 10.0, int(10.0/0.01))
 
 # Power law
 alpha = 2.0
@@ -41,55 +49,23 @@ ll_fwhm = np.log(1.01)
 
 # True parameters
 true_parameters = [amplitude, alpha, white_noise, ll_amplitude, ll_log_x_0, ll_fwhm]
-true_parameters = [ll_amplitude, ll_log_x_0, ll_fwhm]
-
-
-
-# Frequency range
-freq = np.linspace(0.01, 10.0, int(10.0/0.01))
-
-# Set the model parameters and create some fake true data
-_fitter_to_model_params(observation_model, true_parameters)
-psd_shape = observation_model(freq)
-
-# Now randomize the true data
-powers = psd_shape*np.random.chisquare(2, size=psd_shape.shape[0])/2.0
-
-# Make the random data into a Powerspectrum object
-
-ps = Powerspectrum()
-ps.freq = freq
-ps.power = powers
-ps.df = ps.freq[1] - ps.freq[0]
-ps.m = 1
-
-# Define the log-likelihood of the data given the model
-loglike = PSDLogLikelihood(ps.freq, ps.power, observation_model, m=ps.m)
-loglike(true_parameters)
-
-res.print_summary(loglike)
-
-plt.figure(figsize=(12, 8))
-plt.loglog(ps.freq, psd_shape, label="true power spectrum", lw=3)
-plt.loglog(ps.freq, psd_shape_start, label="initial estimate power spectrum", lw=3)
-plt.loglog(ps.freq, ps.power, label="simulated data")
-plt.loglog(ps.freq, res.mfit, label="best fit", lw=3)
-plt.legend()
-plt.show()
-
-
+#true_parameters = [ll_amplitude, ll_log_x_0, ll_fwhm]
 
 
 class SaveParameters:
     """
     Object for the storage of the fitting results
     """
-    def __init__(self, nx, ny, model_name, parameter_names):
+    def __init__(self, nx, ny, observation_model, parameter_names=None):
         self._nx = nx
         self._ny = ny
-        self._model_name = model_name
-        self._np = len(parameter_names)
-        self._parameter_names = parameter_names
+        self._observation_model = observation_model
+        self._model_name = self._observation_model.name
+        if parameter_names is None:
+            self._parameter_names = self._observation_model.param_names
+        else:
+            self._parameter_names = parameter_names
+        self._np = len(self._parameter_names)
         self.p_opt = np.zeros((self._nx, self._ny, self._np))
         self.err = np.zeros_like(self.p_opt)
         self.aic = np.zeros((self._nx, self._ny))
@@ -103,8 +79,20 @@ class SaveParameters:
         self.bic[i, j] = res.bic
         self.result[i, j] = res.result
 
-    def _filename(self):
+    @property
+    def observation_model(self):
+        return self._observation_model
+
+    @property
+    def model_name(self):
         return self._model_name
+
+    @property
+    def parameter_names(self):
+        return self._parameter_names
+
+    def _filename(self):
+        return self.model_name
 
     def _save_basic_information(self, file_path):
         f = os.path.join(file_path, self._filename)
@@ -116,7 +104,7 @@ class SaveParameters:
 nx = 10
 ny = 11
 model_name = 'pl_c_ll'
-sp = SaveParameters(nx, ny, model_name, ['amplitude', 'alpha', 'white_noise', 'll_amplitude', 'll_log_x_0', 'll_fwh'])
+sp = SaveParameters(nx, ny, observation_model, parameter_names=['amplitude', 'alpha', 'white_noise', 'll_amplitude', 'll_log_x_0', 'll_fwhm'])
 for i in range(0, nx):
     for j in range(0, ny):
         # This section will be replaced with a section that reads observed power spectra
@@ -144,11 +132,11 @@ for i in range(0, nx):
 
         # Estimate the starting parameters - will be replaced with a function
         # starting_parameters = starting_parameter_selector(model_name)
-        starting_pars = [ll_amplitude/2.0, -1.0, ll_fwhm/5.0]
+        starting_pars = [amplitude, alpha, white_noise, ll_amplitude, ll_log_x_0, ll_fwhm]
 
         # Do the fit and store the results
         sp.store(i, j, parameter_estimate.fit(loglike, starting_pars))
 
 # Save the results
-sp.save('/Users/Desktop/')
+#sp.save('/Users/Desktop/')
 
