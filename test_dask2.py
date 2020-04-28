d33a1bc1e88bb86319ef2a8afccced1478ff0626
wafels <jackreland@gmail.com>
2020-04-28 13:44:55 -0400

    setting up for a test analysis

--- test_dask2.py
+++ test_dask2.py
@@ -28,34 +28,34 @@ power_law.x_0.fixed = True
 # Constant component
 constant = models.Const1D()
 
-# Gaussian component
+# Lorentz component
 log_lorentz = LogLorentz1D()
+log_lorentz.name = 'log_lorentz'
 
-# Power law plus constant plus Gaussian
-observation_model = log_lorentz
-observation_model.name = 'log_lorentz'
+# All the models
+observation_models = list()
 
-observation_model = power_law + constant + log_lorentz
-observation_model.name = 'pl_c_ll'
-
-observation_models = list(3)
-
-observation_models[0] = power_law + constant
+observation_models.append(power_law + constant)
 observation_models[0].name = 'pl_c'
 
-observation_models[1] = power_law + constant + log_lorentz
+observation_models.append(power_law + constant + log_lorentz)
 observation_models[1].name = 'pl_c_ll'
 
-observation_models[2] = power_law + constant + log_normal
-observation_models[2].name = 'pl_c_ln'
+#observation_models.append(power_law + constant + log_normal)
+#observation_models[2].name = 'pl_c_ln'
 
 
+#############################
+# Load in some power spectra
 
 
-# parameters for fake data.
-
-# Frequency range (normalized so the first one is always 1
+#############################
+# Make some simulated data
 freq = np.linspace(0.01, 10.0, int(10.0/0.01))
+
+# Size of the array
+nx = 50
+ny = 51
 
 # Power law
 alpha = 2.0
@@ -70,10 +70,80 @@ ll_log_x_0 = np.log(1.0)
 ll_fwhm = np.log(1.01)
 
 # True parameters
-true_parameters = [amplitude, alpha, white_noise, ll_amplitude, ll_log_x_0, ll_fwhm]
-#true_parameters = [ll_amplitude, ll_log_x_0, ll_fwhm]
-nx = 10
-ny = 11
+true_parameters = [amplitude, alpha, white_noise]
+
+
+# Create a list of simulated Fourier powers
+# TODO: add the option to create a known range of power law indices, the most important parameter for this study.
+def create_simulated_data(nx, ny, observation_model, true_parameters, freq):
+    powers = []
+    for i in range(0, nx):
+        for j in range(0, ny):
+            # This section will be replaced with a section that reads observed power spectra
+            # Set the model parameters
+            _fitter_to_model_params(observation_model, true_parameters)
+
+            # Create the true data
+            psd_shape = observation_model(freq)
+
+            # Now randomize the true data and store it in an iterable
+            powers.append(psd_shape * np.random.chisquare(2, size=psd_shape.shape[0]) / 2.0)
+    return powers
+
+
+powers = create_simulated_data(nx, ny, observation_models[0], true_parameters, freq)
+
+
+# TODO: complete this initial parameter estimator
+def initial_parameter_estimate_pl_c(f, p):
+    """
+    Estimate of the power law + constant observation model
+
+    Parameters
+    ----------
+    f :
+
+
+    p :
+
+    Return
+    ------
+
+    """
+    return [amplitude, alpha, white_noise]
+
+
+def dask_fit_fourier_pl_c(powers):
+    """
+    Fits the power law + constant observation model
+
+    Parameters
+    ----------
+    powers :
+
+    Return
+    ------
+
+    """
+
+    # Make the random data into a Powerspectrum object
+    ps = Powerspectrum()
+    ps.freq = freq
+    ps.power = powers
+    ps.df = ps.freq[1] - ps.freq[0]
+    ps.m = 1
+
+    # Define the log-likelihood of the data given the model
+    loglike = PSDLogLikelihood(ps.freq, ps.power, observation_models[0], m=ps.m)
+
+    # Parameter estimation object
+    parameter_estimate = PSDParEst(ps, fitmethod="L-BFGS-B", max_post=False)
+
+    # Estimate the starting parameters - will be replaced with a function
+    # starting_parameters = starting_parameter_selector(model_name)
+    initial_parameter_estimate = initial_parameter_estimate_pl_c(ps.freq, ps.power)
+
+    return parameter_estimate.fit(loglike, initial_parameter_estimate)
 
 
 def dask_fit_fourier(powers):
@@ -103,32 +173,36 @@ def dask_fit_fourier(powers):
     return parameter_estimate.fit(loglike, starting_pars)
 
 
-# Create an array of Fourier powers
-powers = []
-t_start = Time.now()
-for i in range(0, nx):
-    for j in range(0, ny):
-        # This section will be replaced with a section that reads observed power spectra
-        # Set the model parameters
-        _fitter_to_model_params(observation_model, true_parameters)
-
-        # Create the true data
-        psd_shape = observation_model(freq)
-
-        # Now randomize the true data and store it in an iterable
-        powers.append(psd_shape * np.random.chisquare(2, size=psd_shape.shape[0]) / 2.0)
-
-
+# TODO: Create plots of the output
+# TODO: 1. Histograms of values
+# TODO: 2. Spatial maps of values
+# TODO: 3. Scatter plots of one value versus another
 if __name__ == '__main__':
-    client = distributed.Client()
+
     # Use Dask to to fit the spectra
+    client = distributed.Client()
     print('Dask processing of {:n} spectra'.format(nx*ny))
-    results = client.map(dask_fit_fourier, powers)
+
+    # Get the start time
+    t_start = Time.now()
+
+    # Do the model fits
+    results = client.map(dask_fit_fourier_pl_c, powers)
     z = client.gather(results)
+
+    # Get the end time and calculate the time taken
     t_end = Time.now()
-    print('Time taken ', (t_end-t_start).to(u.s))
+    print('Time taken to ', (t_end-t_start).to(u.s))
 
     # Now go through all the results and save out the results
+    # Total number of outputs = 2*n_parameters + 3
+    # For short hand call n_parameters 'n' instead
+    # 0 : n-1 : parameter values
+    # n : 2n-1 : error in parameter values
+    # 2n : AIC
+    # 2n + 1 : BIC
+    # 2n + 2 : result
+    # TODO: put all the output into the same three dimensional array
     n_parameters = len(z[0].p_opt)
     p_opt = np.zeros((nx, ny, n_parameters))
     err = np.zeros_like(p_opt)
@@ -147,12 +221,13 @@ if __name__ == '__main__':
             bic[i, j] = r.bic
             result[i, j] = r.result
 
+    # TODO: write out all the output names in the same order as they are stored in the array
     # If they are referred via a dictionary it is easier to automate their saving
     output = {'p_opt': p_opt, 'err': err, 'aic': aic, 'bic': bic, 'result': result}
 
     # Save the arrays - to be analyzed by other programs
     for output_type in list(output.keys()):
-        filename = '{:s}.{:s}.npz'.format(observation_model.name, output_type)
+        filename = '{:s}.{:s}.npz'.format(observation_models[0].name, output_type)
         filepath = os.path.join(directory, filename)
         print('Saving ' + filepath)
         np.savez(filepath, output[output_type])
